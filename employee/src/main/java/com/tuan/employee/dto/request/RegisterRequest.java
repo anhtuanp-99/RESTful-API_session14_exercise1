159bbf6b98d6b243b365bb0cc4390e62e489f6d5
anhtuanp-99 <anhtuanhl2004@gmail.com>
2026-08-15 23:50:16 +0700

    update

--- employee/src/main/java/com/tuan/employee/dto/request/RegisterRequest.java
+++ employee/src/main/java/com/tuan/employee/dto/request/RegisterRequest.java
@@ -7,7 +7,8 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class LoginRequest {
+public class RegisterRequest {
     private String username;
     private String password;
+    private String fullName;
 }
--- employee/src/main/java/com/tuan/employee/model/User.java
+++ employee/src/main/java/com/tuan/employee/model/User.java
@@ -1,15 +1,12 @@
 package com.tuan.employee.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 
 @Entity
 @Table(name = "users")
-@Data
+@Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
